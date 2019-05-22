c9f942c20d6cbd7c7f873f3632461fb421f1971d
Tristan <trjahnke@gmail.com>
2019-05-22 12:42:11 -0500

    login, logout, register, hashing added

---
+++ flaskblog/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+# Will be an env later with new key
+app.config['SECRET_KEY'] = 'e83889d49dabe0e9b204a70e20a0aa90'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskblog import routes
